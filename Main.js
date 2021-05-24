de5b3524b2cc0fe9f93dc8f75f32d59c46a64162
SydeJX <sydeweiz@gmail.com>
2021-05-24 22:46:52 +0300

    Update Main.js

--- Main.js
+++ Main.js
@@ -3,29 +3,33 @@ function OnClickEvent(form){
 var käyttäjätunnus = form.Käyttäjätunnus.value;
 var password = form.KäyttäjäSalasana.value;
 var sähköposti = form.sähköposti.value;
-
-
+var result = 0;
+  var ehdot = /\S+@\S+/;
 //onko väärin laitettu jotakin (tarkistus)
 if (käyttäjätunnus.length < 5) {
 document.getElementById('varoitus').innerHTML = "Käyttäjätunnuksessa saa olla enintään 5 merkkiä";
-  var result = true;
+  var result = 1;
+} else {
+  document.getElementById('varoitus').innerHTML = "";
 }
 
 if (password.length < 5) {
 document.getElementById('varoitus1').innerHTML = "Salsanassa saa olla enintään 5 merkkiä";
-  var result = true;
+  var result = 1;
+} else {
+  document.getElementById('varoitus1').innerHTML = "";
 }
 
 if (sähköposti.length == "") {
   document.getElementById('varoitus2').innerHTML = "Sähköposti ei voi olla tyhjä";
-  var result = true;
+  var result = 1;
+} else if (!ehdot.test(sähköposti)){
+  document.getElementById('varoitus2').innerHTML = "Väärä sähköposti";
+  var result = 1;
+} else {
+    document.getElementById('varoitus2').innerHTML = "";
 }
 
-if (!ehdot.test(sähköposti)){
-  var ehdot = /\S+@\S+/;
-  document.getElementById('varoitus2').innerHTML = "Väärä sähköposti";
-  var result = true;
-}
-if (result == true)
+if (result === 1)
 return false;
 }
