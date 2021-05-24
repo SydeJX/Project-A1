f50fa7e59859b23308be702898f417d7b23b9f5f
SydeJX <sydeweiz@gmail.com>
2021-05-24 12:50:11 +0300

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,33 +1,8 @@
-  function OnCLickEvent() {
-    //Ottaa id
-    function tarkista(form)
-    {
-    var sähköposti = form.sähköposti.value;
-    var käyttäjätunnus = form.Käyttäjätunnus.value;
-    var käyttäjäSalasana = form.KäyttäjäSalasana.value;
+function OnClickEvent(){
+var käyttäjätunnus = document.getElementById(tunnus);
 
-//Tämä katsoo jos olet kirjoitanut mitään
-    if (käyttäjätunnus.value == "") {
-      alert("Et ole kirjoitanut mitään tunnusta");
-      if (käyttäjätunnus.length < 5){
-        alert("Vähintää viisi kirjainta")
-      }
-  } else if (käyttäjäSalasana.length < 5) {
-        alert("Vähintää viisi kirjainta")
-  } else if (sähköposti.value == ""){
-      alert("Et ole kirjoitanut mitään Sähköpostia");
-      let Mail = sähköposti.value;
-      var ehdot = /\S+@\S+/;
-      if (!ehdot.test(Mail))
-      {
-        alert("Anna Kunnon Sähköposti");
-      }
+if (käyttäjätunnus.value < 5) {
+  alert("Käyttäjätunnuksessa saa olla enintään 5 merkkiä");
 
-  } else {
-      if (checkbox1.checked == true) {
-        //qe
-      }
-
-    }
-  }
+}
 }
