e74a90e99a9691b92c5fb23e00f0708f88041cf4
SydeJX <sydeweiz@gmail.com>
2021-05-24 22:03:49 +0300

    Update Main.js

--- Main.js
+++ Main.js
@@ -4,25 +4,28 @@ var käyttäjätunnus = form.Käyttäjätunnus.value;
 var password = form.KäyttäjäSalasana.value;
 var sähköposti = form.sähköposti.value;
 
+
 //onko väärin laitettu jotakin (tarkistus)
 if (käyttäjätunnus.length < 5) {
 document.getElementById('varoitus').innerHTML = "Käyttäjätunnuksessa saa olla enintään 5 merkkiä";
-return false;
+  var result = true;
 }
 
 if (password.length < 5) {
 document.getElementById('varoitus1').innerHTML = "Salsanassa saa olla enintään 5 merkkiä";
-return false;
+  var result = true;
 }
 
 if (sähköposti.length == "") {
   document.getElementById('varoitus2').innerHTML = "Sähköposti ei voi olla tyhjä";
-  return false;
+  var result = true;
 }
 
 if (!ehdot.test(sähköposti)){
   var ehdot = /\S+@\S+/;
   document.getElementById('varoitus2').innerHTML = "Väärä sähköposti";
-  return false;
+  var result = true;
 }
+if (result == true)
+return false;
 }
